12c4ade7cbe80b19fa028292f79740e0e885b434
Lawrence <macknever@gmail.com>
2021-10-20 14:42:18 -0700

    use config file to set the port

--- src/main/java/net/globalrelay/vertx/MainVerticle.java
+++ src/main/java/net/globalrelay/vertx/MainVerticle.java
@@ -2,14 +2,22 @@ package net.globalrelay.vertx;
 
 import org.jgroups.util.UUID;
 
+import io.vertx.config.ConfigRetriever;
+import io.vertx.config.ConfigRetrieverOptions;
+import io.vertx.config.ConfigStoreOptions;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.AsyncResult;
+import io.vertx.core.Handler;
+import io.vertx.core.Promise;
+import io.vertx.core.json.JsonObject;
+import io.vertx.core.net.impl.AsyncResolveConnectHelper;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.RoutingContext;
 
 public class MainVerticle extends AbstractVerticle {
 
     @Override
-    public void start() {
+    public void start(Promise<Void> start) {
 
         vertx.deployVerticle(new HelloVerticle());
 
@@ -19,15 +27,29 @@ public class MainVerticle extends AbstractVerticle {
 
         router.get("/api/v1/hello/:name").handler(this::helloNameHandler);
 
-        int httpPort;
+        ConfigStoreOptions defaultCOnfig = new ConfigStoreOptions().setType("file").setFormat("json")
+                .setConfig(new JsonObject().put("path", "config.json"));
 
-        try {
-            httpPort = Integer.parseInt(System.getProperty("http.port", "8080"));
-        } catch (NumberFormatException nfe) {
-            httpPort = 8080;
+        ConfigRetrieverOptions opts = new ConfigRetrieverOptions().addStore(defaultCOnfig); // can add as many
+                                                                                            // configStores as one wants
+
+        Handler<AsyncResult<JsonObject>> handler = asyncResult -> this.handleConfigResults(start, router, asyncResult);
+        ConfigRetriever cfgRetriever = ConfigRetriever.create(vertx, opts);
+        cfgRetriever.getConfig(handler);
+
+    }
+
+    void handleConfigResults(Promise<Void> start, Router router, AsyncResult<JsonObject> asyncResult) {
+
+        if (asyncResult.succeeded()) {
+            JsonObject config = asyncResult.result();
+            JsonObject http = config.getJsonObject("http");
+            int httpPort = http.getInteger("port");
+            vertx.createHttpServer().requestHandler(router).listen(httpPort);
+            start.complete();
+        } else {
+            start.fail("Unable to load from configuration");
         }
-
-        vertx.createHttpServer().requestHandler(router).listen(httpPort);
 
     }
 
