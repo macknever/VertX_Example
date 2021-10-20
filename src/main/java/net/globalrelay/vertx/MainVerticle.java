5ff34fddf1df4215648d2584804c5a796dc8c002
Lawrence <macknever@gmail.com>
2021-10-20 12:08:23 -0700

    use router to handle path

--- src/main/java/net/globalrelay/vertx/MainVerticle.java
+++ src/main/java/net/globalrelay/vertx/MainVerticle.java
@@ -1,14 +1,28 @@
 package net.globalrelay.vertx;
 
 import io.vertx.core.AbstractVerticle;
+import io.vertx.ext.web.Router;
 
 public class MainVerticle extends AbstractVerticle {
 
     @Override
     public void start() {
-        vertx.createHttpServer().requestHandler(rc -> {
-            rc.response().end("Hello VertX World!");
-        }).listen(8080);
+
+        Router router = Router.router(vertx);
+
+        router.get("/api/v1/hello").handler(rc -> {
+            rc.request().response().end("Hello Vert.X World!");
+        });
+
+        router.get("/api/v1/hello/:name").handler(rc -> {
+
+            String name = rc.pathParam("name");
+            rc.request().response().end(String.format("Hello %s!", name));
+
+        });
+
+        vertx.createHttpServer().requestHandler(router).listen(8080);
+
     }
 
 }
